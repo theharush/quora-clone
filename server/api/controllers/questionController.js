1191e9544f8559628f0bca56771efe5c85048e90
Eyal Harush <theharush@gmail.com>
2020-05-11 16:32:49 +0300

    added client-side Handlebars framework, redisnged index.html

--- server/api/controllers/questionController.js
+++ server/api/controllers/questionController.js
@@ -3,24 +3,24 @@ var mongoose = require('mongoose'),
   Question = mongoose.model('Questions');
 
 //controller for fetching a random question from DB
-exports.getQuestionData = function(req, res) {
-  Question.findOne(function(err, question) {
+exports.getQuestionData = function (req, res) {
+  Question.findOne(function (err, question) {
     if (err)
       res.send(err);
     res.json(question);
   });
 }
 
-exports.postAnswer = function(req,res){
-  Question.findById(req.params.questionId, function(err,question){
-    if(err)
+exports.postAnswer = function (req, res) {
+  Question.findById(req.params.questionId, function (err, question) {
+    if (err)
       res.send(err);
-    
+
     question.Answers.push(req.body);
-    question.save(function(err, updatedQuestion) {
+    question.save(function (err, updatedQuestion) {
       if (err)
         res.send(err);
-      res.json(updatedQuestion);
-  });
-  }) 
+      res.redirect('back');
+    });
+  })
 }
