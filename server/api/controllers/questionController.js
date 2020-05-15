8ea67296663a9392114ce3552dba80d38e4ba991
Eyal Harush <theharush@gmail.com>
2020-05-15 20:53:09 +0300

    fixed a bug where answerer's name is not provided to DB

--- server/api/controllers/questionController.js
+++ server/api/controllers/questionController.js
@@ -1,35 +1,34 @@
 "use strict";
-var mongoose = require('mongoose'),
-  Question = mongoose.model('Questions');
+var mongoose = require("mongoose"),
+  Question = mongoose.model("Questions");
 
 //controller for fetching all questions from DB
-exports.getQuestions = function (req, res) {
-  Question.find().limit(10).exec(function (err, questions) {
-    if (err)
-      res.send(err);
-    res.json(questions);
-  });
-}
+exports.getQuestions = function(req, res) {
+  Question.find()
+    .limit(10)
+    .exec(function(err, questions) {
+      if (err) res.send(err);
+      res.json(questions);
+    });
+};
 
 //controller for fetching a random question from DB
-exports.getQuestion = function (req, res) {
-  Question.findById(req.params.questionId, function (err, question) {
-    if (err)
-      res.send(err);
+exports.getQuestion = function(req, res) {
+  Question.findById(req.params.questionId, function(err, question) {
+    if (err) res.send(err);
     res.json(question);
   });
-}
+};
 
-exports.postAnswer = function (req, res) {
-  Question.findById(req.params.questionId, function (err, question) {
-    if (err)
-      res.send(err);
-    var answer = { Answer: req.body.Answer, Name: req.session.Name };
+exports.postAnswer = function(req, res) {
+  console.log(req.session);
+  Question.findById(req.params.questionId, function(err, question) {
+    if (err) res.send(err);
+    var answer = { Answer: req.body.Answer, Name: req.session.user.Name };
     question.Answers.push(answer);
-    question.save(function (err, updatedQuestion) {
-      if (err)
-        res.send(err);
-      res.redirect('back');
+    question.save(function(err, updatedQuestion) {
+      if (err) res.send(err);
+      res.redirect("back");
     });
-  })
-}
+  });
+};
