fb232838a8325da98fdd2b77e7e8047cb259f4fd
Eyal Harush <theharush@gmail.com>
2020-05-18 19:45:12 +0300

    added a demo login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,54 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+    handleSubmit(e) {
+        alert(`form submitted with username: ${this.state.username} and password :${this.state.password}`);
+        e.preventDefault();
+    }
+    handleInputChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
     render() {
         return (
-            <h1>Login</h1>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Username:
+                    <input
+                        name="username"
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        name="password"
+                        type="text"
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+
+            </form>
         )
     }
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,7 +5,7 @@ export default class NavBar extends Component {
     render() {
         return (
             <nav className="menu">
-                <h1 class="inline brand"><a href="/">Quora</a></h1>
+                <Link to="/"><h1 class="inline brand">Quora</h1></Link>
                 <Link to="/" class="inline">Home</Link>
                 <Link to="/login" class="inline">login</Link>
                 <Link to="/register" class="inline">register</Link>
