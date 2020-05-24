efe5c9818a29c3107478b37f3cee2b43a534529b
Eyal Harush <theharush@gmail.com>
2020-05-24 21:31:53 +0300

    added the abbility to add tags

--- server/api/controllers/questionController.js
+++ server/api/controllers/questionController.js
@@ -4,54 +4,63 @@ var mongoose = require("mongoose"),
   FilterTags = mongoose.model("FilterTags");
 
 //controller for fetching last 10 questions from DB
-exports.getQuestions = function (req, res) {
+exports.getQuestions = function(req, res) {
   Question.find()
     .sort({ created_date: -1 })
     .limit(10)
-    .exec(function (err, questions) {
+    .exec(function(err, questions) {
       if (err) res.send(err);
       res.json(questions);
     });
 };
 
 //controller for fetching last 10 questions after a given date from DB
-exports.getQuestionsbyDateAndTag = function (req, res) {
+exports.getQuestionsbyDateAndTag = function(req, res) {
   let { FilterTag, lastQuestionDate } = req.query;
-  const query = FilterTag === "All" ? { created_date: { $lt: lastQuestionDate } } : { created_date: { $lt: lastQuestionDate }, tag: FilterTag }
+  const query =
+    FilterTag === "All"
+      ? { created_date: { $lt: lastQuestionDate } }
+      : { created_date: { $lt: lastQuestionDate }, tag: FilterTag };
   Question.find(query)
     .sort({ created_date: -1 })
     .limit(10)
-    .exec(function (err, questions) {
+    .exec(function(err, questions) {
       if (err) res.send(err);
       res.json(questions);
     });
 };
 
 //controller for fetching a random question from DB
-exports.getQuestion = function (req, res) {
-  Question.findById(req.params.questionId, function (err, question) {
+exports.getQuestion = function(req, res) {
+  Question.findById(req.params.questionId, function(err, question) {
     if (err) res.send(err);
     else res.json(question);
   });
 };
 
 //controller for posting a new question.
-exports.postQuestion = function (req, res) {
+exports.postQuestion = function(req, res) {
   var question = new Question(req.body);
-  question.save(function (err, question) {
+  question.save(function(err, question) {
     if (err) {
       res.send(err);
-    } else res.send(question);
+    } else {
+      var tag = new FilterTags({ tag: req.body.tag });
+      tag.save(err => {
+        if (err) res.send(err);
+        else res.send(question);
+      });
+    }
   });
 };
 
 //controller for posting an answer on a question
-exports.postAnswer = function (req, res) {
-  Question.findById(req.params.questionId, function (err, question) {
+exports.postAnswer = function(req, res) {
+  Question.findById(req.params.questionId, function(err, question) {
     if (err) res.send(err);
     var answer = { answer: req.body.answer, name: req.user.name };
     question.answers.push(answer);
-    question.save(function (err, updatedQuestion) {
+    question.save(function(err, updatedQuestion) {
       if (err) res.send(err);
       res.send(updatedQuestion);
     });
@@ -59,9 +68,9 @@ exports.postAnswer = function (req, res) {
 };
 
 //controller for getting all filter tags on DB
-exports.getFilterTags = function (req, res) {
-  FilterTags.find().exec(function (err, FilterTags) {
+exports.getFilterTags = function(req, res) {
+  FilterTags.find().exec(function(err, FilterTags) {
     if (err) res.send(err);
     res.json(FilterTags);
-  })
-}
+  });
+};
